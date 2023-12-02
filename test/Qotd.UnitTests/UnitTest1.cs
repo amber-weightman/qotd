6b18e485641725fd4dda3afc6a78e19a1c66f552
Amber <amberweightman@hotmail.com>
2023-12-03 04:03:29 +1100

    Try fix sonarqube

--- test/Qotd.UnitTests/UnitTest1.cs
+++ test/Qotd.UnitTests/UnitTest1.cs
@@ -1,11 +1,16 @@
+using Qotd.Application.Models;
+
 namespace Qotd.UnitTests
 {
     public class UnitTest1
     {
+        // Forcing > 0 coverage to test pipeline
         [Fact]
         public void Test1()
         {
-
+            var md = new Metadata { Values = new Dictionary<string, string>() };
+            var q = new QuestionResponse { Question = "Example", Metadata = md };
+            var r = new RunResponse { RunId = "Example", Metadata = md };
         }
     }
 }
