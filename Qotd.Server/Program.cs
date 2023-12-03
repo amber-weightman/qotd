3a0f55a74e60b6b9fbeec74a764067a9f02fd159
Amber <amberweightman@hotmail.com>
2023-12-04 01:08:04 +1100

    See if updating server http fixes deployed instance

--- Qotd.Server/Program.cs
+++ Qotd.Server/Program.cs
@@ -21,11 +21,11 @@ app.UseDefaultFiles();
 app.UseStaticFiles();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
